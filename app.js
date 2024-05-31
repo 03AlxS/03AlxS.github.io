86c06f38d3b836b6c6690eabe3330cba564823dc
alexis <alexis03rivera@outlook.es>
2024-05-30 19:02:18 -0600

    Chat update

--- app.js
+++ app.js
@@ -1,96 +1,185 @@
-const chatHistory = document.getElementById('chatHistory');
-const inputPrompt = document.getElementById('inputPrompt');
-const sendButton = document.getElementById('sendButton');
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
 
-const apiKey = 'AIzaSyBirPxbUFg9I2NdcZbRHfTEL4hBbQESNno';
-const endpoint = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
+const chatHistory = document.getElementById("chatHistory");
+const inputPrompt = document.getElementById("inputPrompt");
+const sendButton = document.getElementById("sendButton");
+const resetButton = document.getElementById("resetButton");
 
-sendButton.addEventListener('click', sendPrompt);
+const apiKey = "AIzaSyBirPxbUFg9I2NdcZbRHfTEL4hBbQESNno";
+const genAI = new GoogleGenerativeAI(apiKey);
 
-function sendPrompt() {
-  const prompt = inputPrompt.value.trim();
-  if (prompt === '') return;
-
-  addMessageToChat('You', prompt);
-  inputPrompt.value = '';
-  toggleLoadingState(true);
-
-  fetch(endpoint + '?key=' + apiKey, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+let chatSession = genAI
+  .getGenerativeModel({
+    model: "gemini-1.5-pro",
+    systemInstruction:
+      "Eres un asistente virtual para resolver preguntas sobre economía y finanzas.",
+  })
+  .startChat({
+    generationConfig: {
+      temperature: 0.8,
+      topP: 0.95,
+      topK: 64,
+      responseMimeType: "text/plain",
     },
-    body: JSON.stringify({
-      "contents": [
-        {
-          "parts": [
-            {
-              "text": prompt
-            }
-          ]
-        }
-      ]
-    })
-  })
-  .then(response => response.json())
-  .then(data => {
-    toggleLoadingState(false);
-    handleApiResponse(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    toggleLoadingState(false);
-    addMessageToChat('Economia-GPT', 'Lo siento, ocurrió un error al procesar tu solicitud.');
+    safetySettings: [
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ],
+    history: [],
   });
-}
 
-function handleApiResponse(data) {
-  if (data.error) {
-    console.error('Error de API:', data.error);
-    addMessageToChat('Economia-GPT', `Error: ${data.error.message}`);
-  } else if (data.candidates && data.candidates[0] && data.candidates[0].content && data.candidates[0].content.parts) {
-    const generatedText = data.candidates[0].content.parts[0].text;
-    addMessageToChat('Economia-GPT', generatedText);
-  } else {
-    console.error('Respuesta inesperada:', data);
-    addMessageToChat('Economia-GPT', 'Lo siento, ocurrió un error al procesar tu solicitud.');
-  }
-}
+sendButton.addEventListener("click", sendPrompt);
+resetButton.addEventListener("click", resetChat);
 
-function addMessageToChat(sender, message) {
-  const messageElement = document.createElement('div');
-  messageElement.classList.add('chat-message');
-  if (sender === 'You') {
-    messageElement.classList.add('user-message');
-  } else {
-    messageElement.classList.add('bot-message');
-  }
-  messageElement.innerHTML = `<strong>${sender}:</strong> ${markdownToHTML(message)}`;
-  chatHistory.appendChild(messageElement);
-  chatHistory.scrollTop = chatHistory.scrollHeight;
-}
+function formatApiResponse(response) {
+  response = response.trim();
 
-function markdownToHTML(text) {
+  // Convertir Markdown a HTML
   const boldRegex = /\*\*(.*?)\*\*/g;
   const italicRegex = /\*(.*?)\*/g;
   const boldItalicRegex = /\*\*\*(.*?)\*\*\*/g;
   const listRegex = /\* (.*?)(?:\n|$)/g;
 
-  text = text.replace(boldRegex, '<strong>$1</strong>');
-  text = text.replace(italicRegex, '<em>$1</em>');
-  text = text.replace(boldItalicRegex, '<strong><em>$1</em></strong>');
-  text = text.replace(listRegex, '<li>$1</li>');
-  text = text.replace(/<li>(.*?)<\/li>/g, '<ul>$&</ul>');
+  response = response.replace(boldRegex, "<strong>$1</strong>");
+  response = response.replace(italicRegex, "<em>$1</em>");
+  response = response.replace(boldItalicRegex, "<strong><em>$1</em></strong>");
+  response = response.replace(listRegex, "<li>$1</li>");
+  response = response.replace(/<li>(.*?)<\/li>/g, "<ul>$&</ul>");
 
-  return text;
+  return response;
+}
+
+window.addEventListener("load", function () {
+  resetChat();
+  toggleButtonVisibility();
+});
+
+function sendPrompt() {
+  const prompt = inputPrompt.value.trim();
+  if (prompt === "") return;
+
+  addMessageToChat("You", prompt);
+  inputPrompt.value = "";
+  inputPrompt.disabled = true; // Deshabilitar la entrada de texto
+  toggleLoadingState(true);
+
+  chatSession
+    .sendMessage(prompt)
+    .then((result) => {
+      toggleLoadingState(false);
+      const response = result.response.text();
+      const formattedResponse = formatApiResponse(response);
+      addMessageToChat("Economia-GPT", formattedResponse);
+      toggleButtonVisibility();
+      inputPrompt.disabled = false; // Habilitar la entrada de texto nuevamente
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      toggleLoadingState(false);
+      addMessageToChat(
+        "Economia-GPT",
+        "Lo siento, ocurrió un error al procesar tu solicitud."
+      );
+      toggleButtonVisibility();
+      inputPrompt.disabled = false; // Habilitar la entrada de texto nuevamente
+    });
+}
+
+function resetChat() {
+  chatSession = genAI
+    .getGenerativeModel({
+      model: "gemini-1.5-pro",
+      systemInstruction:
+        "Eres un asistente virtual para resolver preguntas sobre economía y finanzas.",
+    })
+    .startChat({
+      generationConfig: {
+        temperature: 0.8,
+        topP: 0.95,
+        topK: 64,
+        maxOutputTokens: 8192,
+        responseMimeType: "text/plain",
+      },
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+        },
+      ],
+      history: [],
+    });
+
+  chatHistory.innerHTML = "";
+  addMessageToChat("Economia-GPT", "¡Hola! ¿En qué puedo ayudarte?");
+  toggleButtonVisibility();
+}
+
+function addMessageToChat(sender, message) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("chat-message");
+  if (sender === "You") {
+    messageElement.classList.add("user-message");
+  } else {
+    messageElement.classList.add("bot-message");
+  }
+  messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+  chatHistory.appendChild(messageElement);
+  chatHistory.scrollTop = chatHistory.scrollHeight;
 }
 
 function toggleLoadingState(isLoading) {
   if (isLoading) {
-    sendButton.classList.add('loading');
+    sendButton.classList.add("loading");
     sendButton.disabled = true;
   } else {
-    sendButton.classList.remove('loading');
-    sendButton.disabled = false;
+    setTimeout(() => {
+      sendButton.classList.remove("loading");
+      sendButton.disabled = false;
+      toggleButtonVisibility();
+    }, 500);
+  }
+}
+
+inputPrompt.addEventListener("input", function () {
+  toggleButtonVisibility();
+});
+
+function toggleButtonVisibility() {
+  if (inputPrompt.value.trim() === "") {
+    sendButton.style.display = "none";
+    resetButton.style.display = "inline-block";
+  } else {
+    sendButton.style.display = "inline-block";
+    resetButton.style.display = "none";
   }
 }
